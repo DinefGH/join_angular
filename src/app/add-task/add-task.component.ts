d8144244d24907d8a4e78ca1a3ba29a72a6cb0d5
DinefGH <112215706+DinefGH@users.noreply.github.com>
2024-04-03 05:57:38 +0200

    assigned to

--- src/app/add-task/add-task.component.ts
+++ src/app/add-task/add-task.component.ts
@@ -2,6 +2,8 @@ import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 import { NgbDateStruct, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { JsonPipe } from '@angular/common';
+import { AddContactService } from 'src/app/services/add-contact.service';
+import { Contact } from 'src/assets/models/contact.model';
 
 @Component({
   selector: 'app-add-task',
@@ -24,12 +26,16 @@ export class AddTaskComponent implements OnInit {
   ];
 
   selectedOption?: { name: string; color: string };  isOpen = false; // Controls the visibility of the dropdown
+  contacts: Contact[] = [];
+  selectedContact: Contact | null = null;
+  isOpenContacts= false;
 
-  constructor(private ngbDateParserFormatter: NgbDateParserFormatter) {}
+  constructor(private ngbDateParserFormatter: NgbDateParserFormatter, private addContactService: AddContactService) {}
 
   ngOnInit(): void {
     const today = new Date();
     this.minDate = { year: today.getFullYear(), month: today.getMonth() + 1, day: today.getDate() };
+    this.loadContacts();
   }
 
   onDateSelect(date: NgbDateStruct): void {
@@ -41,10 +47,43 @@ export class AddTaskComponent implements OnInit {
     this.isOpen = !this.isOpen;
   }
 
+  toggleDropdownContacts(): void {
+    this.isOpenContacts = !this.isOpenContacts;
+  }
+
   selectOption(option: { name: string; color: string }, event: MouseEvent): void {
     event.stopPropagation(); // Prevent the click event from bubbling up to the container
     this.selectedOption = option;
     this.isOpen = false; // Close dropdown after selection
     console.log(`Option selected: ${this.selectedOption.name} with color ${this.selectedOption.color}`);
+  }
+
+  selectContact(contact: Contact, event: MouseEvent): void {
+    this.selectedContact = contact;
+    this.isOpen = false; // Optionally close the dropdown after selection
+    event.stopPropagation(); // Prevent the dropdown from toggling when an option is selected
+  }
+
+
+  loadContacts(): void {
+    this.addContactService.getContacts().subscribe({
+      next: (contacts) => {
+        this.contacts = contacts;
+      },
+      error: (error) => {
+        console.error('Failed to load contacts:', error);
+      }
+    });
+  }
+
+  getInitials(name: string | undefined): string {
+    if (!name) return ''; // This will catch both undefined and empty string cases
+  
+    let initials = name.split(' ')
+                      .filter((n) => n !== '')
+                      .map((n) => n[0]?.toUpperCase() ?? '') // Use ?. in case a split result is an empty string
+                      .slice(0, 2); // Only take the first two parts for initials
+    
+    return initials.join('');
   }
 }
